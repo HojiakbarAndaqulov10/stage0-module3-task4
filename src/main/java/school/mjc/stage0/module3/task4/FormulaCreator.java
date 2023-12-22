186865739d0e3419074534146ea95c5ae46d530e
Your Name <you@example.com>
2023-12-22 16:08:12 +0500

    zor

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+
+        // Formula: result = x + y
+        int result = x + y;
+
+        // Print the result
+        System.out.println("Result: " + result);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int[] charCodes = {99, 104, 97, 114};
+
+        for (int code : charCodes) {
+            System.out.println((char) code);
+        }
     }
 }
